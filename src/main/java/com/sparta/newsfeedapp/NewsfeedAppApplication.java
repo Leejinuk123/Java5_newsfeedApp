07879c20650597f8b6b7b2c60d49d9f580bfbc92
leejinuk <leejinouk123@kakao.com>
2024-06-05 20:26:30 +0900

    fix: API 수정

--- src/main/java/com/sparta/newsfeedapp/NewsfeedAppApplication.java
+++ src/main/java/com/sparta/newsfeedapp/NewsfeedAppApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories
+@EnableJpaAuditing
 @SpringBootApplication
 public class NewsfeedAppApplication {
     public static void main(String[] args) {
