f954df6f78bed47989d51d2daa1c69de1b35008b
yoon <pyh051920@gmail.com>
2024-06-07 18:43:47 +0900

    api/user -> api/users, db수정하는 내용들 수정, 유저 삭제시 상태변경 및 각종 기능 불가,*** User.java:비번수정 싫을시 null체크 수정해야함

--- src/main/java/com/sparta/newsfeedapp/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/newsfeedapp/config/WebSecurityConfig.java
@@ -75,7 +75,7 @@ public class WebSecurityConfig {
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                         .requestMatchers("/api/users/signup").permitAll()
                         .requestMatchers("/api/users/login").permitAll()
-                        .requestMatchers("/api/users/refresh-token").permitAll()
+                        .requestMatchers("/api/auth/refresh-token").permitAll()
                         // 서버 단에서 에러가 발생시 아래 url이 에러창을 띄워준다
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
